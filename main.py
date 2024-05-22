bf6ec4b9b605e1c9d91487970929e51437732395
Justin Chiu <justinchiu@g.harvard.edu>
2024-05-22 01:36:19 -0400

    finished

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import torch
 import runningstats
+from welford_torch import Welford
 
+x = torch.randn(100, 1000)
+
+v = runningstats.Variance()
+w = Welford()
+for y in x:
+    v.add(y[None])
+    w.add(y[None])
+
+true = x.var(0).sum()
+runningstats_var = v.variance().sum()
+welford_var = w.var_s.sum()
+
+print(true)
+print(runningstats_var)
+print(welford_var)
